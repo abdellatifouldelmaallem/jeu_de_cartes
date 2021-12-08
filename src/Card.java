a3297f7a0072fc244e01ce1e2e75598f8e0232b3
abdellatif <ouldeabdellatif40@gmail.com>
2021-12-08 10:22:41 +0100

    i compared between objects

--- src/Card.java
+++ src/Card.java
@@ -1,13 +1,15 @@
-
+import java.util.Objects;
 
 public class Card {
 
 	private String name;
 	private String value;
+	private int point;
 	
-	public Card(String newName, String newValue) {
+	public Card(String newName, String newValue, int newPoint) {
 		this.setName(newName);
 		this.setValue(newValue);
+		this.setPoint(newPoint);
 	}
 	
 	
@@ -20,7 +22,7 @@ public class Card {
 	
 	@Override
 	public String toString() {
-		return "[ " + name + " , " + value +" ]";
+		return "[ " + name + " , " + value + " , " + point + " ]";
 	}
 
 
@@ -30,6 +32,40 @@ public class Card {
 	public void setValue(String value) {
 		this.value = value;
 	}
+
+
+	public int getPoint() {
+		return point;
+	}
+
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, value);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		return Objects.equals(name, other.name) && Objects.equals(value, other.value) && Objects.equals(point, other.point);
+	}
+	
+	
+	
+	
+	
 
 	
 
--- src/PlaceOfGame.java
+++ src/PlaceOfGame.java
@@ -1,51 +1,112 @@
 import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.Collections;
 
 public class PlaceOfGame {
 
 	public static void main(String[] args) {
 
-		 // create arrayList Global 
-		 ArrayList<Card> RandomeArray = new ArrayList<Card>();
-		 
-		 RandomeArray.add(new Card("land","white"));
-		 RandomeArray.add(new Card("land","bleu"));
-		 RandomeArray.add(new Card("land","black"));
-		 RandomeArray.add(new Card("land","red"));
-		 
-		 RandomeArray.add(new Card("creature","40"));
-		 RandomeArray.add(new Card("creature","30"));
-		 RandomeArray.add(new Card("creature","20"));
-		 RandomeArray.add(new Card("creature","10"));
-		 
-		 RandomeArray.add(new Card("spells","text"));
-		 RandomeArray.add(new Card("spells","texttext"));
-		 RandomeArray.add(new Card("spells","texttexttext"));
-		 RandomeArray.add(new Card("spells","texttexttexttext"));
-		 
+		// create arrayList Global
+		ArrayList<Card> RandomeArray = new ArrayList<Card>();
+
+		RandomeArray.add(new Card("land", "white", 60));
+		RandomeArray.add(new Card("land", "bleu",  50));
+		RandomeArray.add(new Card("land", "black", 30));
+		RandomeArray.add(new Card("land", "red",   20));
+
+		RandomeArray.add(new Card("creature", "40" , 40));
+		RandomeArray.add(new Card("creature", "30" , 30));
+		RandomeArray.add(new Card("creature", "20" , 20));
+		RandomeArray.add(new Card("creature", "10" , 10));
+
+		RandomeArray.add(new Card("spell", "text",1));
+		RandomeArray.add(new Card("spell", "texttext",2));
+		RandomeArray.add(new Card("spell", "texttexttext",3));
+		RandomeArray.add(new Card("spell", "texttexttexttext",4));
+
+		// call players
+
+		Player player1 = new Player("youssef", RandomeArray);
+		player1.myShuffle();
+		ArrayList<Card> cardsForYoussef = new ArrayList<Card>();
+		cardsForYoussef.addAll(RandomeArray);
+		System.out.println(cardsForYoussef);
+
+		Player player2 = new Player("abdo", RandomeArray);
+		player2.myShuffle();
+		ArrayList<Card> cardsForAbdo = new ArrayList<Card>();
+		cardsForAbdo.addAll(RandomeArray);
+		System.out.println(cardsForAbdo);
+		// change to array of string
+
+		// compare two objects
+		int scoreForPlayer1 = 0;
+		int scoreForPlayer2 = 0;
+		for (int i = 0; i < 12; i++) {
+
+			Card youssef = cardsForYoussef.get(i);
+			Card abdo = cardsForAbdo.get(i);
+
+			if (!youssef.getName().equals(abdo.getName())) {
+
+				if (youssef.getName().equals("spell")) {
+					scoreForPlayer1++;
+				} else if (youssef.getName().equals("creature") && !abdo.getName().equals("spell")) {
+					scoreForPlayer1++;
+				} else if (abdo.getName().equals("creature") && !youssef.getName().equals("spell")) {
+					scoreForPlayer1++;
+				} else {
+					scoreForPlayer2++;
+				}
+
+			} else {
+				// count length
+				if (youssef.getName().equals("spell") && abdo.getName().equals("spell")) {
+					int YousssefLength = youssef.getValue().length();
+					int AbdoLength = abdo.getValue().length();
+					if (YousssefLength > AbdoLength) {
+						scoreForPlayer1++;
+					} else {
+						scoreForPlayer2++;
+					}
+			    // count integer 
+				} else if (youssef.getName().equals("creature") && abdo.getName().equals("creature")) {
+					if(youssef.getPoint() == abdo.getPoint()) {
+						scoreForPlayer1++;
+						scoreForPlayer2++;
+					}
+					else if(youssef.getPoint()>abdo.getPoint()) {
+						scoreForPlayer1++;
+					}else {
+						scoreForPlayer2++;
+					}
+                // count color
+				} else if (youssef.getName().equals("land") && abdo.getName().equals("land")) {
+					if(youssef.getPoint() == abdo.getPoint()) {
+						scoreForPlayer1++;
+						scoreForPlayer2++;
+					}
+					else if(youssef.getPoint()>abdo.getPoint()) {
+						scoreForPlayer1++;
+					}else {
+						scoreForPlayer2++;
+					}
+				}
+					
+			}
+			
 		
-		// System.out.println(RandomeArray);
-		 
-		 // call players 
-		 
-		 Player player1 = new Player("youssef",RandomeArray );
-		 player1.myShuffle();
-		 
-		 ArrayList<Card> cardsOfPlayer1 = new ArrayList<Card>();
-		 cardsOfPlayer1.addAll(player1.getCards());
-		 
-		 while(cardsOfPlayer1.size()>0) {
-			 ArrayList<Card> oneCard = new ArrayList<Card>();
-			 oneCard.add(cardsOfPlayer1.get(0));
-			 cardsOfPlayer1.remove(0);
-			 
-			 System.out.println(oneCard);
-		 }
-		 
-		 
-		
-		 
-		
-	} 
+
+		}
+		if(scoreForPlayer1>scoreForPlayer2) {
+			System.out.println("the person who is win is : youssef");
+		}else if(scoreForPlayer2>scoreForPlayer1) {
+			System.out.println("the person who is win is : Abdo");
+		}else {
+			System.out.println("no one win !!");
+		}
+
+	}
 
 }
